305ca2460a43f1af6a8f6611ba1ccdf12175990d
petervanrijn <petervanrijn@hotmail.nl>
2021-03-24 14:41:59 +0100

    styling nummer toevoegen af

--- resources/views/nummers/nummers.blade.php
+++ resources/views/nummers/nummers.blade.php
@@ -2,6 +2,11 @@
 @section('content')
     <h1>Nummers</h1>
     <ul class="list">
+        <li class="list__item">
+            <h2>Nummer</h2>
+            <h2>Artiest</h2>
+            <h2>Genre</h2>
+        </li>
         @include('nummers.components.nummer--index')
     </ul>
 @stop
