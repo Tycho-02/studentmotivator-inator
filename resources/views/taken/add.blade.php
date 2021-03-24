73174370c20f7036e4b884bcec7c0639e802530a
Tycho-02 <tycho05.vanveen@gmail.com>
2021-03-24 14:47:20 +0100

    started styling

--- resources/views/taken/add.blade.php
+++ resources/views/taken/add.blade.php
@@ -7,33 +7,26 @@
 
 <!-- Content -->
 @section('content')
-    <h1>Taak Toevoegen:</h1>
-    <form method='POST' action="/taaktoevoegen">
+    <h1 class="add__header">Taak Toevoegen:</h1>
+    <form class="" method='POST' action="/taaktoevoegen">
         @csrf
         @method('POST')
-        <div>
-            <label for="title">Taak:</label>
-            <input type="text" name="title" placeholder="Nieuw taak" required/>
-        </div>
-        <div>
-            <label for="omschrijving">Omschrijving:</label>
-            <textarea name="omschrijving" rows="5" placeholder="Taak omschrijving...."></textarea>
-        </div>
-        <div>
-            <label for="label">Label:</label>
-            <input type="text" name="label" placeholder="label" />
-        </div>
-        <div>
-            <label for="deadline">Deadline:</label>
-            <input type="date" name="deadline" value="{{date('Y-m-d', time() + 86400)}}" min="{{date('Y-m-d')}}" />
-        </div>
-        <div>
-            <label for="uitvoerdatum">Uitvoerdatum:</label>
-            <input type="date" name="uitvoerdatum" value="{{date('Y-m-d')}}" min="{{date('Y-m-d')}}" />
-        </div>
-        <div>
-            <input type="submit" value="Taak toevoegen"/>
-        </div>
+            <label class="form__label" for="title">Taak</label>
+            <input class="form__field" type="text" name="title" placeholder="Nieuw taak" required/>
+        
+            <label class="form__label" for="omschrijving">Omschrijving</label>
+            <textarea class="form__field" name="omschrijving" rows="5" placeholder="Taak omschrijving...."></textarea>
+        
+            <label class="form__label" for="label">Label</label>
+            <input class="form__field" type="text" name="label" placeholder="Label" />
+        
+            <label class="form__label" for="deadline">Deadline</label>
+            <input class="form__field" type="date" name="deadline" value="{{date('Y-m-d', time() + 86400)}}" min="{{date('Y-m-d')}}" />
+
+            <label class="form__label" for="uitvoerdatum">Uitvoerdatu:</label>
+            <input class="form__field" type="date" name="uitvoerdatum" value="{{date('Y-m-d')}}" min="{{date('Y-m-d')}}" />
+
+            <input class="content--button__actions__primary" type="submit" value="Taak toevoegen"/>
 
     </form>
 @endsection
