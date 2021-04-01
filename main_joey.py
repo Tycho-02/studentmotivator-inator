b5435fb489ecc2690cedfc8afb4a9e3f6791d774
Joey Peschier <s1120743@student.hsleiden.nl>
2021-04-01 22:32:08 +0200

    einde dag push voor als mijn huis vannacht afbrand

--- main_joey.py
+++ main_joey.py
@@ -1,7 +1,7 @@
 import mysql.connector
+from datetime import *
 import time
 import requests
-
 import serial
 import os
 
@@ -14,24 +14,28 @@ mydb = mysql.connector.connect(
 
 port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
 
-buddyName = "Test"
-buddySkin = ""
-idealTemp = ""
 
+today = str(date.today())
 
 mycursor = mydb.cursor()
 while True:
     rcv = port.readline().strip()
-    mycursor.execute("SELECT * from studiebuddy;")
+    selectstring = "SELECT * from studiebuddy, studieplanning WHERE studieplanning.userId = 1 AND studieplanning.datum = %s"
+    vals = (today,)
+    mycursor.execute(selectstring, vals)
+    selectvals =  ()
     for x in mycursor:    
         buddyName = x[3]
         buddySkin = x[4]
         idealTemp = int(x[5])
-
+        idealHum = int(x[6])
+        idealLight = int(x[7])
+        planning = x[11]
     
     stuurNaam = bytes(buddyName)
     stuurSkin = bytes(buddySkin)
     stuuridealTemp = bytes(idealTemp)
+    stuurPlanning = bytes(planning)
     port.write('\n')
     time.sleep(0.1)
     port.write(stuurNaam)
@@ -41,6 +45,9 @@ while True:
     port.write('\n')
     time.sleep(0.1)
     port.write(stuuridealTemp)
+    port.write('\n')
+    time.sleep(0.1)
+    port.write(stuurPlanning)
     port.write('\n')
     time.sleep(0.1)
 
