4be350d9b1053d96a6e355d812d5e45d64cf4a9f
Tycho-02 <tycho05.vanveen@gmail.com>
2021-04-07 10:50:23 +0200

    fixed menu issue

--- resources/views/taken/taakDefault.blade.php
+++ resources/views/taken/taakDefault.blade.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css'>
     <link rel="stylesheet" href="/css/main.css">
     <title>@yield('title')</title>
 </head>
 <body>
+    <button class="menu--button__action" onclick="window.location='/'" ><i class="fas fa-long-arrow-alt-left fa-2x"></i></button>
     @yield('content')
 </body>
 </html>
