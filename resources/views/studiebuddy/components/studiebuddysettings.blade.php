6ccccc8bbb347b1872a20960343c89a19a0a4c56
Joey Peschier <s1120743@student.hsleiden.nl>
2021-04-13 19:41:37 +0200

    studiebuddy form styling gedaan

--- resources/views/studiebuddy/components/studiebuddysettings.blade.php
+++ resources/views/studiebuddy/components/studiebuddysettings.blade.php
@@ -3,45 +3,63 @@
 @endsection
 
 @section('content')
-    <section class="studiebuddyFormWrapper">
-    <form action="/updatestudiebuddy" class="form" method="POST">
+    <section class="studiebuddySettings">
+    <form action="/updatestudiebuddy" class="studiebuddySettingsForm" method="POST">
     <h1>Studiebuddy Instellingen</h1>
         @csrf
         @method('POST')
-            <section class="form__group">
-                <label class="form__label" for="Naam">Naam</label>
-                <input type="form__field" type="text" name="naam" value="{{$studiebuddy->naam}}" maxlength="10">
+            <section class="studiebuddySettingsForm__group">
+                <label for="Naam">Naam</label>
+                <input type="text" class="studiebuddySettingsForm__field" type="text" name="naam" value="{{$studiebuddy->naam}}" maxlength="10">
             </section>
-            <section class="form__group">
-                <label class="form__label" for="Naam">Latitude</label>
-                <input type="form__field" id="js--latitude" type="number" name="lat" value="{{$studiebuddy->lat}}">
-                <label class="form__label" for="Naam">Longitude</label>
-                <input type="form__field" id="js--longitude" type="number" name="long" value="{{$studiebuddy->long}}">
-                <button type="button" onclick="getLocation()">Locatie Ophalen</button>
+            <section class="studiebuddySettingsForm__group">
+                <h2 class="studiebuddySettingsForm_groupheader">Locatie</h2>
+                <label for="Naam">Latitude</label>
+                <input class="studiebuddySettingsForm__field" type="number" id="js--latitude" type="number" name="lat" value="{{$studiebuddy->lat}}">
+                <label for="Naam">Longitude</label>
+                <input class="studiebuddySettingsForm__field" type="number" id="js--longitude" type="number" name="long" value="{{$studiebuddy->long}}">
+                <button class="studiebuddySettingsForm__field studiebuddySettingsForm__field--button" type="button" onclick="getLocation()">Locatie Ophalen</button>
             </section>
-            <section class="form__group form__group--icons">
-                <div class="form__iconselect">
-                    <img class="form__icon" src="/img/snorlaxicon.png" alt="">
-                    <input type="radio" id="Snorlax" name="skin" value="Snorlax" {{ ($studiebuddy->skin=="Snorlax")? "checked" : "" }}>
+            <section class="studiebuddySettingsForm__group studiebuddySettingsForm__group--radio">
+                <h2 class="studiebuddySettingsForm_groupheader">Icoontje selecteren</h2>
+                <div class="studiebuddySettingsForm__radioWrapper">
+                    <div class="studiebuddySettingsForm__radio">
+                        <img class="form__icon" src="/img/ladybugicon.png" alt="">
+                        <input type="radio" id="Ladybug" name="skin" value="Ladybug" {{ ($studiebuddy->skin=="Ladybug")? "checked" : "" }}>
+                    </div>
+                    <div class="studiebuddySettingsForm__radio">
+                        <img class="form__icon" src="/img/turtleicon.png" alt="">
+                        <input type="radio" id="Turtle" name="skin" value="Turtle" {{ ($studiebuddy->skin=="Turtle")? "checked" : "" }}>
+                    </div>
+                    <div class="studiebuddySettingsForm__radio">
+                        <img class="form__icon" src="/img/lionicon.png" alt="">
+                        <input type="radio" id="Lion" name="skin" value="Lion" {{ ($studiebuddy->skin=="Lion")? "checked" : "" }}>
+                    </div>
                 </div>
-                <div class="form__iconselect">
-                    <img class="form__icon" src="/img/lionicon.png" alt="">
-                    <input type="radio" id="Lion" name="skin" value="Lion" {{ ($studiebuddy->skin=="Lion")? "checked" : "" }}>
+                <div class="studiebuddySettingsForm__radioWrapper studiebuddySettingsForm__radioWrapper--center">
+                    <div class="studiebuddySettingsForm__radio">
+                        <img class="form__icon" src="/img/snorlaxicon.png" alt="">
+                        <input type="radio" id="Snorlax" name="skin" value="Snorlax" {{ ($studiebuddy->skin=="Snorlax")? "checked" : "" }}>
+                    </div>
+                    <div class="studiebuddySettingsForm__radio">
+                        <img class="form__icon" src="/img/charizardicon.png" alt="">
+                        <input type="radio" id="Charizard" name="skin" value="Charizard" {{ ($studiebuddy->skin=="Charizard")? "checked" : "" }}>
+                    </div>
                 </div>
-                <div class="form__iconselect">
-                    <img class="form__icon" src="/img/ladybugicon.png" alt="">
-                    <input type="radio" id="Ladybug" name="skin" value="Ladybug" {{ ($studiebuddy->skin=="Ladybug")? "checked" : "" }}>
-                </div>
+                
             </section>
-            <section class="form__group form__group--slider">
-                <label for="temp" class="form__label">Temperatuur</label>
-                    <div class="form__sliderholder"><p>15</p><input class="form__range" type="range" id="temp" name="temp" min="15" max="25" step="1" value="{{$studiebuddy->ideale_temp}}"><p>25</p></div>
-                <label for="temp" class="form__label">Luchtvochtigheid</label>
-                    <div class="form__sliderholder"><p>10</p><input class="form__range" type="range" id="luchtvochtigheid" name="luchtvochtigheid" min="10" max="90" step="5" value="{{$studiebuddy->ideale_luchtvochtigheid}}"><p>90</p></div>
+            <section class="studiebuddySettingsForm__group">
+                <label for="temp">Temperatuur</label>
+                <div class="studiebuddySettingsForm__rangeHolder"><p>15&#8451;</p><input class="studiebuddySettingsForm__field" type="range" id="temp" name="temp" min="15" max="25" step="1" value="{{$studiebuddy->ideale_temp}}"><p>25&#8451;</p></div>
             </section>
-            <section class="form__group">
-                <input type="submit">
+            <section class="studiebuddySettingsForm__group">
+                <label for="temp">Luchtvochtigheid</label>
+                    <div class="studiebuddySettingsForm__rangeHolder"><p>10%</p><input class="studiebuddySettingsForm__field" type="range" id="luchtvochtigheid" name="luchtvochtigheid" min="10" max="90" step="5" value="{{$studiebuddy->ideale_luchtvochtigheid}}"><p>90%</p></div>
+            </section>
+            <section class="studiebuddySettingsForm__group">
+                <input class="studiebuddySettingsForm__field studiebuddySettingsForm__field--button" type="submit">
             </section>
     </form>
     </section>
+    
 @endsection
